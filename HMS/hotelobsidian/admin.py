5cfbc666ecb138e43e2f26959dae66ff808b6c2d
Maryam Saqib <122818826+maryam-saqib@users.noreply.github.com>
2023-09-19 21:26:08 +0500

    Added tables to admin page
    On admin page, we can delete, update, add new items in the tables

--- HMS/hotelobsidian/admin.py
+++ HMS/hotelobsidian/admin.py
@@ -2,3 +2,34 @@ from django.contrib import admin
 
 # Register your models here.
 
+from .models import Menu
+
+admin.site.register(Menu)
+
+from .models import Customer
+
+admin.site.register(Customer)
+
+from .models import Hotel
+
+admin.site.register(Hotel)
+
+from .models import Room
+
+admin.site.register(Room)
+
+from .models import Roomtype
+
+admin.site.register(Roomtype)
+
+from .models import Salesandoffers
+
+admin.site.register(Salesandoffers)
+
+from .models import Booking
+
+admin.site.register(Booking)
+
+from .models import Billing
+
+admin.site.register(Billing)
