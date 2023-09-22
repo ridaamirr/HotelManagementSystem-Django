1dcbd3cf09da4599fadc4190653b321522411f8c
Maryam Saqib <122818826+maryam-saqib@users.noreply.github.com>
2023-09-22 23:51:53 +0500

    Removed old tables from admin.py

--- HMS/hotelobsidian/admin.py
+++ HMS/hotelobsidian/admin.py
@@ -2,10 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Menu
-
-admin.site.register(Menu)
-
 from .models import Customer
 
 admin.site.register(Customer)
@@ -21,10 +17,6 @@ admin.site.register(Room)
 from .models import Roomtype
 
 admin.site.register(Roomtype)
-
-from .models import Salesandoffers
-
-admin.site.register(Salesandoffers)
 
 from .models import Booking
 
