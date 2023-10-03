ea17cb6ad2a7d689694e404650ad790002f27e38
Isbah Malik <129406085+Isbah-23@users.noreply.github.com>
2023-10-03 20:37:07 +0500

    Login for Customers should work now

--- HMS/hotelobsidian/views.py
+++ HMS/hotelobsidian/views.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Customer
+from django.contrib import messages
 
 # Create your views here
 
 def default(request):
-    # Render the HTML template index.html with the data in the context variable
     return render(request, 'default.html') 
 
 def login(request):
-    # Render the HTML template index.html with the data in the context variable
     return render(request, 'login.html')
+
+def user_dashboard(request):
+    username = request.GET.get('username')
+    password = request.GET.get('password')
+    #print("Username:", username)
+    #print("Password:", password)
+    #sys.exit()
+    try:
+        user_data_row = Customer.objects.get(pk = username,password = password)
+    except Customer.DoesNotExist:
+        messages.error(request, "Invalid information entered")
+        return redirect(request.META.get('HTTP_REFERER', 'default'))
+    return render(request, 'user/dashboard.html')
