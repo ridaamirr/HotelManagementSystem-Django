7cd5b8696cfa5d6f04fd777203e1c3d744826994
Isbah Malik <129406085+Isbah-23@users.noreply.github.com>
2023-10-05 02:01:15 +0500

    Import error resolution

--- HMS/hotelobsidian/models.py
+++ HMS/hotelobsidian/models.py
@@ -17,80 +17,10 @@ class AdminLogin(models.Model):
         db_table = 'admin_login'
 
 
-class AuthGroup(models.Model):
-    name = models.CharField(unique=True, max_length=150)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group'
-
-
-class AuthGroupPermissions(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group_permissions'
-        unique_together = (('group', 'permission'),)
-
-
-class AuthPermission(models.Model):
-    name = models.CharField(max_length=255)
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
-    codename = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_permission'
-        unique_together = (('content_type', 'codename'),)
-
-
-class AuthUser(models.Model):
-    password = models.CharField(max_length=128)
-    last_login = models.DateTimeField(blank=True, null=True)
-    is_superuser = models.IntegerField()
-    username = models.CharField(unique=True, max_length=150)
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
-    email = models.CharField(max_length=254)
-    is_staff = models.IntegerField()
-    is_active = models.IntegerField()
-    date_joined = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user'
-
-
-class AuthUserGroups(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_groups'
-        unique_together = (('user', 'group'),)
-
-
-class AuthUserUserPermissions(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_user_permissions'
-        unique_together = (('user', 'permission'),)
-
-
 class Billing(models.Model):
     billing_id = models.AutoField(db_column='Billing_ID', primary_key=True)  # Field name made lowercase.
     user_id = models.CharField(db_column='User_ID', max_length=15, blank=True, null=True)  # Field name made lowercase.
     status = models.TextField(db_column='Status', blank=True, null=True)  # Field name made lowercase.
-    history = models.IntegerField(db_column='History', blank=True, null=True)  # Field name made lowercase.
 
     class Meta:
         managed = False
@@ -102,7 +32,6 @@ class Booking(models.Model):
     billing = models.ForeignKey(Billing, models.DO_NOTHING, db_column='Billing_ID', blank=True, null=True)  # Field name made lowercase.
     currentdate = models.DateField(db_column='CurrentDate', blank=True, null=True)  # Field name made lowercase.
     numberofdays = models.IntegerField(db_column='NumberOfDays', blank=True, null=True)  # Field name made lowercase.
-    hasrated = models.TextField(db_column='HasRated', blank=True, null=True)  # Field name made lowercase.
     isbooked = models.TextField(db_column='isBooked', blank=True, null=True)  # Field name made lowercase.
 
     class Meta:
@@ -119,39 +48,10 @@ class Customer(models.Model):
     address = models.TextField(db_column='Address', blank=True, null=True)  # Field name made lowercase.
     dob = models.DateField(db_column='DOB', blank=True, null=True)  # Field name made lowercase.
     password = models.TextField(db_column='Password')  # Field name made lowercase.
-    paymenthistory = models.FloatField(db_column='PaymentHistory', blank=True, null=True)  # Field name made lowercase.
-    securityquestion1 = models.ForeignKey('Securityquestions', models.DO_NOTHING, db_column='SecurityQuestion1', blank=True, null=True)  # Field name made lowercase.
-    answer1 = models.TextField(db_column='Answer1', blank=True, null=True)  # Field name made lowercase.
-    securityquestion2 = models.ForeignKey('Securityquestions', models.DO_NOTHING, db_column='SecurityQuestion2', related_name='customer_securityquestion2_set', blank=True, null=True)  # Field name made lowercase.
-    answer2 = models.TextField(db_column='Answer2', blank=True, null=True)  # Field name made lowercase.
 
     class Meta:
         managed = False
         db_table = 'customer'
-
-
-class DjangoAdminLog(models.Model):
-    action_time = models.DateTimeField()
-    object_id = models.TextField(blank=True, null=True)
-    object_repr = models.CharField(max_length=200)
-    action_flag = models.PositiveSmallIntegerField()
-    change_message = models.TextField()
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'django_admin_log'
-
-
-class DjangoContentType(models.Model):
-    app_label = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'django_content_type'
-        unique_together = (('app_label', 'model'),)
 
 
 class DjangoMigrations(models.Model):
@@ -165,16 +65,6 @@ class DjangoMigrations(models.Model):
         db_table = 'django_migrations'
 
 
-class DjangoSession(models.Model):
-    session_key = models.CharField(primary_key=True, max_length=40)
-    session_data = models.TextField()
-    expire_date = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_session'
-
-
 class Hotel(models.Model):
     branch_id = models.AutoField(db_column='Branch_ID', primary_key=True)  # Field name made lowercase.
     location = models.TextField(db_column='Location', blank=True, null=True)  # Field name made lowercase.
@@ -183,29 +73,6 @@ class Hotel(models.Model):
     class Meta:
         managed = False
         db_table = 'hotel'
-
-
-class Menu(models.Model):
-    food_id = models.AutoField(db_column='Food_ID', primary_key=True)  # Field name made lowercase.
-    name = models.TextField(db_column='Name', blank=True, null=True)  # Field name made lowercase.
-    cuisine = models.TextField(db_column='Cuisine', blank=True, null=True)  # Field name made lowercase.
-    description = models.TextField(db_column='Description', blank=True, null=True)  # Field name made lowercase.
-    price = models.IntegerField(db_column='Price', blank=True, null=True)  # Field name made lowercase.
-    image = models.TextField(db_column='Image', blank=True, null=True)  # Field name made lowercase.
-
-    class Meta:
-        managed = False
-        db_table = 'menu'
-
-
-class Orders(models.Model):
-    food_id = models.IntegerField(db_column='Food_ID', blank=True, null=True)  # Field name made lowercase.
-    billing_id = models.IntegerField(db_column='Billing_ID', blank=True, null=True)  # Field name made lowercase.
-    quantity = models.IntegerField(db_column='Quantity', blank=True, null=True)  # Field name made lowercase.
-
-    class Meta:
-        managed = False
-        db_table = 'orders'
 
 
 class Room(models.Model):
@@ -226,38 +93,7 @@ class Roomtype(models.Model):
     type = models.TextField(db_column='Type', blank=True, null=True)  # Field name made lowercase.
     price = models.IntegerField(db_column='Price', blank=True, null=True)  # Field name made lowercase.
     image = models.TextField(db_column='Image', blank=True, null=True)  # Field name made lowercase.
-    rating = models.FloatField(db_column='Rating', blank=True, null=True)  # Field name made lowercase.
-    noofusersrated = models.IntegerField(db_column='NoOfUsersRated', blank=True, null=True)  # Field name made lowercase.
 
     class Meta:
         managed = False
         db_table = 'roomtype'
-
-
-class Salesandoffers(models.Model):
-    offer_id = models.AutoField(db_column='Offer_ID', primary_key=True)  # Field name made lowercase.
-    occasion = models.TextField(db_column='Occasion', blank=True, null=True)  # Field name made lowercase.
-    percentage = models.FloatField(db_column='Percentage', blank=True, null=True)  # Field name made lowercase.
-    image = models.TextField(db_column='Image', blank=True, null=True)  # Field name made lowercase.
-
-    class Meta:
-        managed = False
-        db_table = 'salesandoffers'
-
-
-class Securityquestions(models.Model):
-    question_id = models.AutoField(db_column='Question_ID', primary_key=True)  # Field name made lowercase.
-    text = models.TextField(db_column='Text')  # Field name made lowercase.
-
-    class Meta:
-        managed = False
-        db_table = 'securityquestions'
-
-
-class Selectedoffers(models.Model):
-    offer_id = models.IntegerField(db_column='Offer_ID', blank=True, null=True)  # Field name made lowercase.
-    billing_id = models.IntegerField(db_column='Billing_ID', blank=True, null=True)  # Field name made lowercase.
-
-    class Meta:
-        managed = False
-        db_table = 'selectedoffers'
