09c572088fdfb5cd51241e819ba0b595f454fc9c
ridaamirr <ridaamirbashir@gmail.com>
2023-10-07 13:44:20 +0500

    Automatic Checkout added

--- HMS/hotelobsidian/views.py
+++ HMS/hotelobsidian/views.py
@@ -1,9 +1,12 @@
 from .models import *
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect 
+from django.db import connection 
 
 # Create your views here
 
-def default(request):
+def default(request): 
+    cursor=connection.cursor()
+    cursor.execute('call AutomaticCheckOut()')  
     return render(request, 'default.html') 
 
 def login(request):
@@ -12,7 +15,7 @@ def login(request):
 def signup(request): 
     if request.method == 'POST': 
         if request.POST.get('cnic') and request.POST.get('firstname') and request.POST.get('lastname') and request.POST.get('email') and request.POST.get('phonenumber') and request.POST.get('password') and request.POST.get('dob') and request.POST.get('address'):
-            saverecord=Customer();
+            saverecord=Customer()
             saverecord.cnic=request.POST.get('cnic')
             saverecord.firstname=request.POST.get('firstname')
             saverecord.lastname=request.POST.get('lastname')
@@ -21,6 +24,6 @@ def signup(request):
             saverecord.dob=request.POST.get('dob')
             saverecord.address=request.POST.get('address')
             saverecord.phonenumber=request.POST.get('phonenumber') 
-            saverecord.save();
+            saverecord.save()
             return render(request,'login.html')      
-    return render(request, 'signup.html')
+        return render(request,'signup.html')
