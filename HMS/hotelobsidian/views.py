db5589848eb4f7c85df6ff8e011e7ab2df17c56f
haisemishere <91388126+haisemishere@users.noreply.github.com>
2023-10-14 21:53:11 +0500

    check availability fixed

--- HMS/hotelobsidian/views.py
+++ HMS/hotelobsidian/views.py
@@ -18,20 +18,26 @@ def default(request):
          }     
     return render(request, 'default.html', context) 
 
-def checkavailabilty(request): 
-    if request.method == 'POST': 
-        cursor = connection.cursor()  
-        data=(request.POST.get('Location'),'Deluxe',request.POST.get('noofbeds'))  #TYPE HARDCODED
-        print(data)
-        cursor.callproc('CheckAvailability',data)
-        result=cursor.fetchall() 
-        temptohide=1
+from django.http import JsonResponse
+
+def checkavailabilty(request):
+    if request.method == 'POST':
+        location = request.POST.get('Location')
+        room_type = request.POST.get('Type')
+        no_of_beds = request.POST.get('noofbeds')
+
+        cursor = connection.cursor()
+        data = (location, room_type, no_of_beds)
+        cursor.callproc('CheckAvailability', data)
+        result = cursor.fetchall()
+        temptohide = 1
         if result:
-            return render(request,'default.html',{'result':result,'temptohide':temptohide} )   
-        else: 
-            return render(request,'default.html',{'result':result,'temptohide':temptohide} ) 
-    else: 
-        return render(request,'default.html')  
+            return render(request, 'default.html', {'result': result, 'temptohide': temptohide})
+        else:
+            return render(request, 'default.html', {'result': result, 'temptohide': temptohide})
+    else:
+        return render(request, 'default.html')
+
 
 
 
